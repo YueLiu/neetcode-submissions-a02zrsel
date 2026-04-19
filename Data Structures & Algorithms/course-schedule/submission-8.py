bcec5ad2465894b20b416de5e07c57dea495c6a8
Yue Liu <liuyue0027@gmail.com>
2026-04-18 23:57:37 -0700

    Add: course-schedule - submission-8

---
+++ Data Structures & Algorithms/course-schedule/submission-8.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        # DAG
+        preMap = {i: [] for i in range(numCourses)}
+        for c, p in prerequisites:
+            preMap[c].append(p)
+
+        current_path = set()
+        finish = set()
+
+        def dfs(c):
+            if c in current_path:
+                return False
+            if preMap[c] == []:
+                return True
+            if c in finish:
+                return True
+            
+            current_path.add(c)
+            for p in preMap[c]:
+                if not dfs(p):
+                    return False
+            current_path.remove(c)
+            finish.add(c)
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+        return True
+
+
