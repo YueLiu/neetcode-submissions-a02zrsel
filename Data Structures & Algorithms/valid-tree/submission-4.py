f80921da3c332c39119c6f080a03f546e49702be
Yue Liu <liuyue0027@gmail.com>
2026-04-20 20:58:23 -0700

    Add: valid-tree - submission-4

---
+++ Data Structures & Algorithms/valid-tree/submission-4.py
@@ -0,0 +1,36 @@
+from collections import deque
+from typing import List
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # no loop and all connected
+
+        if len(edges) != n-1:
+            return False
+
+        adj = [[] for _ in range(n)]
+        for v1, v2 in edges:
+            adj[v1].append(v2)
+            adj[v2].append(v1)
+
+        q = deque()
+        q.append([0,-1])
+        visited = set()
+        visited.add(0)
+        
+        while q:
+            c, p = q.popleft()
+            for nei in adj[c]: # RENAMED to 'nei'
+                if nei != p and nei not in visited:
+                    q.append([nei, c])
+                    visited.add(nei)
+                elif nei != p and nei in visited:
+                    return False
+                    
+        return len(visited) == n # Now 'n' safely retains its original value
+
+
+
+        
+
+        
