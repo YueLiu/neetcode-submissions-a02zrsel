446e8166db8312311b398e68b80acb96146bd5b6
Yue Liu <liuyue0027@gmail.com>
2026-05-06 11:33:13 -0700

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        from functools import cache
+        @cache
+        def dfs(i):
+            if i == len(s):
+                return True
+            for word in wordDict:
+                if i+len(word) <= len(s) and s[i:i+len(word)] == word:
+                    if dfs(i+len(word)):
+                        return True
+            return False
+
+        return dfs(0)
